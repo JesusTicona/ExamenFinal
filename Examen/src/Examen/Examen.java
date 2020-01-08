35e0f94aafdf11e782606c1692dbabf99a8845bf
JesusTicona <59584493+JesusTicona@users.noreply.github.com>
2020-01-08 10:24:21 -0500

    Add files via upload

---
+++ Examen/src/Examen/Examen.java
@@ -0,0 +1,16 @@
+package Examen;
+
+public class Examen {
+	public static double Suma(double a, double b) {
+		return a+b;
+	}
+	public static double Resta(double a,double b) {
+		return a-b;
+	}
+	public static double Multiplicacion(double a,double b) {
+		return a*b;
+	}
+	public static double Division(double a,double b) {
+		return a/b;
+	}
+}
